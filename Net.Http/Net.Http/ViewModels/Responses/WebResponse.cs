ce7b1289442259c3fa97ad6de7a4f34fe79bdbef
Murat YavuzKAPLAN <murat@yavuzkaplan.com>
2021-08-20 22:09:12 +0300

    Non-generic version of WebResponse added

--- Net.Http/Net.Http/ViewModels/Responses/WebResponse.cs
+++ Net.Http/Net.Http/ViewModels/Responses/WebResponse.cs
@@ -9,8 +9,8 @@ namespace Back.Zone.Net.Http.ViewModels.Responses
         TA? Payload
     ) where TA : class
     {
-        private const string SuccessMessage = "#SUCCEED";
-        private const string FailureMessage = "#FAILED";
+        private const string SuccessMessage = "#SUCCEED#";
+        private const string FailureMessage = "#FAILED#";
 
         public static WebResponse<TA> Succeed() => new(true, SuccessMessage, DateTime.Now, default);
 
@@ -28,5 +28,33 @@ namespace Back.Zone.Net.Http.ViewModels.Responses
             new(false, exception.Message, DateTime.Now, default);
 
         public static WebResponse<TA> FailedBecause(TA payload) => new(false, FailureMessage, DateTime.Now, payload);
+    }
+
+    public sealed record WebResponse(
+        bool Result,
+        string Message,
+        DateTime HappenedAt,
+        object Payload
+    )
+    {
+        private const string SuccessMessage = "#SUCCEED#";
+        private const string FailureMessage = "#FAILED#";
+
+        public static WebResponse Succeed() => new(true, SuccessMessage, DateTime.Now, default);
+
+        public static WebResponse SucceedWith(string message) =>
+            new(true, message, DateTime.Now, default);
+
+        public static WebResponse SucceedWith(object payload) =>
+            new(true, SuccessMessage, DateTime.Now, payload);
+
+        public static WebResponse Failed() => new(false, FailureMessage, DateTime.Now, default);
+
+        public static WebResponse FailedBecause(string message) => new(false, message, DateTime.Now, default);
+
+        public static WebResponse FailedBecause(Exception exception) =>
+            new(false, exception.Message, DateTime.Now, default);
+
+        public static WebResponse FailedBecause(object payload) => new(false, FailureMessage, DateTime.Now, payload);
     }
 }
