d6398b3e02a72760520664e71fe2ab8c15c36ac2
Murat YavuzKAPLAN <murat@yavuzkaplan.com>
2022-06-08 16:12:06 +0300

    PaginationParameters PageSize has a min and max amount

--- Net.Http/Net.Http/TransferObjects/HttpRequestObjects/PaginationParameters.cs
+++ Net.Http/Net.Http/TransferObjects/HttpRequestObjects/PaginationParameters.cs
@@ -3,11 +3,27 @@ using System.Text.Json.Serialization;
 namespace Back.Zone.Net.Http.TransferObjects.HttpRequestObjects;
 
 public sealed record PaginationParameters(
-    [property: JsonPropertyName("page")] int Page,
-    [property: JsonPropertyName("page_size")]
-    int PageSize
+    [property: JsonPropertyName("page")] int Page
 )
 {
+    private readonly int _pageSize;
+
+    [property: JsonPropertyName("page_size")]
+    public int PageSize
+    {
+        get => _pageSize;
+
+        init
+        {
+            _pageSize = value switch
+            {
+                0 => 10,
+                > 100 => 100,
+                _ => value
+            };
+        }
+    }
+
     public int QueryPage => Page switch
     {
         0 => 1,
@@ -23,7 +39,7 @@ public sealed record PaginationParameters(
     };
 
     public static PaginationParameters BuildFrom(int page, int pageSize) =>
-        new(page, pageSize);
+        new(page) { PageSize = pageSize };
 
     public static PaginationParameters WithDefaults() => BuildFrom(1, 10);
 }
